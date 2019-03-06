8b22fa3513a8ecdd21c5f9445d90716a2fa50779
tituspetrisor <petrisortit@gmail.com>
2019-03-06 19:59:42 +0200

    parametrized tests

--- src/test/java/org/fasttrackit/SearchTest.java
+++ src/test/java/org/fasttrackit/SearchTest.java
@@ -3,6 +3,8 @@ package org.fasttrackit;
 import org.fasttrackit.pageobjects.Header;
 import org.fasttrackit.pageobjects.ProductGrid;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,17 +12,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class SearchTest extends TestBase {
+    private String keyword;
+
+    public SearchTest(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Parameterized.Parameters
+    public static List<String> inputData(){
+        return Arrays.asList("vase", "camera");
+    }
 
     @Test
     public void searchByOneKeywordTest() {
 
-        String keyword = "vase";
         Header header = PageFactory.initElements(driver, Header.class);
         header.search(keyword);
 
